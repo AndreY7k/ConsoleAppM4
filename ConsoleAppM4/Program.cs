9ba53a3a92dcc118c31d54f4b5aed3e8e2126498
Andrei.K <kavardakovandrei01@icloud.com>
2023-04-16 11:36:25 +0900

    Выполнено Задание 4.2.11

--- ConsoleAppM4/Program.cs
+++ ConsoleAppM4/Program.cs
@@ -173,6 +173,51 @@ namespace ConsoleAppM4
                     Console.WriteLine("Your color is yellow!");
                     break;
             }
+
+            //Задание 4.2.11
+            int t = 0; 
+
+            do
+            {
+                Console.WriteLine(t);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+                    default:
+
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+
+                t++;
+            } while (t < 3);
         }
     }
 }
