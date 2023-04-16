cce717964178b68a8d90457bbbe1e02f4055251e
Andrei.K <kavardakovandrei01@icloud.com>
2023-04-16 13:32:33 +0900

    Добавлена остановка цикла

--- ConsoleAppM4/Program.cs
+++ ConsoleAppM4/Program.cs
@@ -36,7 +36,7 @@ namespace ConsoleAppM4
             Console.WriteLine(result);
             Console.ReadKey(); 
             
-             */
+             
 
             //Задание 4.1.12
 
@@ -219,7 +219,48 @@ namespace ConsoleAppM4
 
                 t++;
             } while (t < 3);
+            */
+            int k = 0;
 
+            while (true)
+            {
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                Console.WriteLine(k);
+
+                var text = Console.ReadLine();
+
+                if (text == "stop")
+                {
+                    Console.WriteLine("Цикл остановлен");
+                    break;
+                }
+
+                switch (text)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        continue;
+                }
+            }
         }
     }
 }
