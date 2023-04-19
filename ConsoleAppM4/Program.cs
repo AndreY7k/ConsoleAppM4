4bbe8a576ba2a87dd18883b8cd085bd51a8a4b82
Andrei.K <kavardakovandrei01@icloud.com>
2023-04-19 21:01:30 +0900

    Выполнено задание 4.3.7

--- ConsoleAppM4/Program.cs
+++ ConsoleAppM4/Program.cs
@@ -261,6 +261,17 @@ namespace ConsoleAppM4
                         continue;
                 }
             }
+            //Задание 4.3.7
+
+            Console.WriteLine("Напишите ваше имя");
+
+            var MyName = Console.ReadLine();
+
+            for (int i = MyName.Length - 1; i >= 0; i--)
+            {
+                Console.Write(MyName[i] + " ");
+            }
+            Console.ReadKey();
         }
     }
 }
