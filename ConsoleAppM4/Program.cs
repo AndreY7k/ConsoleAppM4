4de45db96094e2fc13d9ec98fed5f8e1021a26cb
Andrei.K <kavardakovandrei01@icloud.com>
2023-05-30 10:32:17 +0900

    Add Task4.3.14

--- ConsoleAppM4/Program.cs
+++ ConsoleAppM4/Program.cs
@@ -9,19 +9,7 @@ namespace ConsoleAppM4
     {
         static void Main(string[] args)
         {
-            int[][] array = new int[3][];
-            array[0] = new int[2] { 1, 2 };
-            array[1] = new int[3] { 1, 2, 3 };
-            array[2] = new int[5] { 1, 2, 3, 4, 5 };
 
-            foreach (var i in array)
-            {
-                foreach (var j in i)
-                {
-                    Console.Write(j + " ");
-                }
-            }
-            Console.ReadLine();
         }
     }
 }
