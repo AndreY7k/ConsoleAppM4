252abd546bee14368e89e8095b88474f14d54188
Andrei.K <kavardakovandrei01@icloud.com>
2023-05-30 10:43:21 +0900

    Add Task4.3.15

--- ConsoleAppM4/Task4.3.15.cs
+++ ConsoleAppM4/Task4.3.15.cs
@@ -1,9 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
-using System.Net.Sockets;
-using System.Net.WebSockets;
-using System.Security.Cryptography;
-
+﻿/*
 namespace ConsoleAppM4
 {
     internal class Program
@@ -13,14 +8,15 @@ namespace ConsoleAppM4
             int[] array = new int[] { 1, -2, 5, -6, -3, 12, 45, 67, -78, 15, -89 };
             int sum = 0;
             for (int i = 0; i < array.Length; i++)
-            {                
+            {
                 if (array[i] > 0)
                 {
                     sum++;
-                }                
+                }
             }
             Console.WriteLine(sum);
             Console.ReadKey();
         }
     }
 }
+*/
