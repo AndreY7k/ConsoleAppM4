0344ff775f2804f3a5fc4d3e32158a9ee84d7a32
Andrei.K <kavardakovandrei01@icloud.com>
2023-05-31 10:08:23 +0900

    changes in section 4.3.17

--- ConsoleAppM4/Program.cs
+++ ConsoleAppM4/Program.cs
@@ -10,30 +10,7 @@ namespace ConsoleAppM4
     {
         static void Main(string[] args)
         {
-            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-
-            for (int i = 0; i <= arr.GetUpperBound(0); i++)
-            {
-                for (int j = 0; j <= arr.GetUpperBound(1); j++)
-                {
-                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
-                    {
-                        if (arr[i, j] > arr[i, k])
-                        {
-                            var array = arr[i, k];
-                            arr[i, k] = arr[i, j];
-                            arr[i, j] = array;
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
-            {
-                for (int k = 0; k < arr.GetUpperBound(1) + 1; k++)
-                    Console.Write(arr[i, k] + " ");
-
-                Console.WriteLine();
-            }
+            
         }
     }
 }
--- ConsoleAppM4/Task4.3.17.cs
+++ ConsoleAppM4/Task4.3.17.cs
@@ -5,6 +5,8 @@ namespace ConsoleAppM4
     {
         static void Main(string[] args)
         {
+
+            //От меньшего к большему
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 
             for (int i = 0; i <= arr.GetUpperBound(0); i++)
@@ -27,6 +29,33 @@ namespace ConsoleAppM4
                 for (int k = 0; k < arr.GetUpperBound(1) + 1; k++)
                     Console.Write(arr[i, k] + " ");
 
+                Console.WriteLine();
+            }
+
+            //ОТ большего к меньшему 
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    {
+                        if (arr[i, j] < arr[i, k])
+                        {
+                            var sum = arr[i, j];
+                            arr[i, j] = arr[i, k];
+                            arr[i, k] = sum;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < arr.GetUpperBound(0) + 1;  i++)
+            {
+                for(int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                {
+                    Console.Write(arr[i, j] + " "); 
+                }
                 Console.WriteLine();
             }
         }
