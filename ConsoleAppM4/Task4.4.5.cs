7045c2266a2305c337c4252eb7b8f2e51c7ac5f7
Andrei.K <kavardakovandrei01@icloud.com>
2023-05-31 11:10:31 +0900

    Add Task4.4.5.

---
+++ ConsoleAppM4/Task4.4.5.cs
@@ -0,0 +1,28 @@
+﻿/*
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace ConsoleAppM4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age, int NameCount) pet;
+
+            Console.Write("Введите имя вашего питомца: ");
+            pet.Name = Console.ReadLine();
+            Console.Write("Введите тип питомца: ");
+            pet.Type = Console.ReadLine();
+            Console.Write("Введите возраст питомца: ");
+            pet.Age = Convert.ToInt32(Console.ReadLine());
+            pet.NameCount = pet.Name.Length;
+            Console.WriteLine($"Кол-во букв в имени вашего питомца: {pet.NameCount}");
+        }
+    }
+}
+*/
